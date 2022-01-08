a56e2264ad4fb3249bde17ff3babc18e8fee03a8
EneyaFrancis <bsc-com-05-19@unima.ac.mw>
2022-01-08 12:47:35 +0800

    Registration package

---
+++ CodePlatformApi/src/main/java/com/example/CodePlatformApi/Registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.example.CodePlatformApi.Registration;
+
+public class RegistrationService {
+    public static String register(RegistrationRequest request) {
+        return "A OKAY! :)";
+    }
+}
