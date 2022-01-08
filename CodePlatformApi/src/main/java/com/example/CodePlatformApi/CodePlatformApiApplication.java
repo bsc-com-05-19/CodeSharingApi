76cadade9ea40d8feb939efe45050686846e2b23
EneyaFrancis <bsc-com-05-19@unima.ac.mw>
2022-01-08 11:37:21 +0800

    initial commit

---
+++ CodePlatformApi/src/main/java/com/example/CodePlatformApi/CodePlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.CodePlatformApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodePlatformApiApplication.class, args);
+	}
+
+}
---
+++ CodePlatformApi/src/test/java/com/example/CodePlatformApi/CodePlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodePlatformApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
