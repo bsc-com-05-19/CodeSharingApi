cbc120db70ab79a167e20a03d5b7861dddccc3c2
EneyaFrancis <bsc-com-05-19@unima.ac.mw>
2022-01-08 22:01:22 +0800

    email validation and saving the user

--- CodePlatformApi/src/main/java/com/example/CodePlatformApi/Registration/RegistrationService.java
+++ CodePlatformApi/src/main/java/com/example/CodePlatformApi/Registration/RegistrationService.java
@@ -1,10 +1,33 @@
 package com.example.CodePlatformApi.Registration;
 
+import com.example.CodePlatformApi.appuser.AppUser;
+import com.example.CodePlatformApi.appuser.AppUserRole;
+import com.example.CodePlatformApi.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private  static   AppUserService appUserService;
+   private static EmailValidator emailValidator;
+
     public static String register(RegistrationRequest request) {
-        return "A OKAY! :)";
+
+        boolean isValidEmail = emailValidator .test(request.getEmail());
+        if(!isValidEmail){
+            throw  new IllegalStateException("An invalid email :/...");
+        }
+
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
--- CodePlatformApi/src/main/java/com/example/CodePlatformApi/appuser/AppUser.java
+++ CodePlatformApi/src/main/java/com/example/CodePlatformApi/appuser/AppUser.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.*;
 import java.util.Collection;
 import java.util.Collections;
-
 @Getter
 @Setter
 @EqualsAndHashCode
@@ -30,8 +29,8 @@ public class AppUser implements UserDetails {
             generator = "user_sequence"
     )
     private  Long Id;
-    private  String name;
-    private  String username;
+    private  String firstName;
+    private  String lastName;
     private  String email;
     private  String password;
     @Enumerated(EnumType.STRING)
@@ -39,20 +38,16 @@ public class AppUser implements UserDetails {
     private  Boolean locked;
     private  Boolean enabled;
 
-    public AppUser(String name,
-                   String username,
+    public AppUser(String firstName,
+                   String lastName,
                    String email,
                    String password,
-                   AppUserRole appUserRole,
-                   Boolean locked,
-                   Boolean enabled) {
-        this.name = name;
-        this.username = username;
+                   AppUserRole appUserRole) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.appUserRole = appUserRole;
-        this.locked = locked;
-        this.enabled = enabled;
     }
 
     @Override
@@ -68,7 +63,15 @@ public class AppUser implements UserDetails {
 
     @Override
     public String getUsername() {
-        return username;
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 
     @Override
